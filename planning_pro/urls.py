b45e1d0bc68cb2af854e66d06240b6d8bcdcbbf9
magattefall806 <magatte.fall22@isep-thies.edu.sn>
2024-10-04 10:12:53 +0200

    Ajout de l'interface admin

--- planning_pro/urls.py
+++ planning_pro/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('gestion_emploi_du_temps.urls')),
 ]
